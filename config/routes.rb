fe1b814a1fd1859e748609fe735f188fdc8028a7
robertoles <robertoles@me.com>
2014-02-10 14:12:01 +0000

    Adding product routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,33 @@ Spree::Core::Engine.routes.append do
   
   namespace :admin do
     resources :products do
+      # adding in a bunch of routes elsewhere, this is the last gem thats loaded, and it overwrites other routes used, making them all fail
+      get :related, :on => :member
+      resources :relations
+        
+      collection do
+        get :search
+      end
+
+      resources :product_properties do
+        collection do
+          post :update_positions
+        end
+      end
+      resources :images do
+        collection do
+          post :update_positions
+        end
+      end
+      member do
+        get :clone
+      end
+      resources :variants do
+        collection do
+          post :update_positions
+        end
+      end
+      
       resources :videos do
         collection do
           post :update_positions
